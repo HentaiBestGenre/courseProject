1df364b067b154599f15e903b008d9de280013a7
Hentai <97235307+HentaiBestGenre@users.noreply.github.com>
2022-04-20 19:37:54 +0300

    Role Administration, does not work correctly

--- courseProject/Controllers/AccountController.cs
+++ courseProject/Controllers/AccountController.cs
@@ -33,33 +33,20 @@ namespace courseProject.Controllers
                 User user = await userManager.FindByEmailAsync(details.Email);
                 if(user != null)
                 {
-                    Console.WriteLine($"Found{user.Email}");
                     await signInManager.SignOutAsync();
                     var result = await signInManager.PasswordSignInAsync(user, details.Password, false, false);
-                    if (result.Succeeded) { Console.WriteLine($"if auth {User.Identity.IsAuthenticated}"); return Redirect("~/Home/Index"); }
+                    if (result.Succeeded) { return Redirect("~/Home/Index"); }
                 }
                 ModelState.AddModelError(nameof(LoginModel.Email), "Invalid user or password");
             }
             return View(details);
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Logout(LoginModel details, string returnUrl)
+        [Authorize]
+        public async Task<IActionResult> Logout()
         {
-            if (ModelState.IsValid)
-            {
-                User user = await userManager.FindByEmailAsync(details.Email);
-                if (user != null)
-                {
-                    Console.WriteLine($"Found{user.Email}");
-                    await signInManager.SignOutAsync();
-                    var result = await signInManager.PasswordSignInAsync(user, details.Password, false, false);
-                    if (result.Succeeded) { Console.WriteLine($"if auth {User.Identity.IsAuthenticated}"); return Redirect("~/Home/Index"); }
-                }
-                ModelState.AddModelError(nameof(LoginModel.Email), "Invalid user or password");
-            }
-            return View(details);
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- courseProject/Models/UserViewModels.cs
+++ courseProject/Models/UserViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace courseProject.Models
 {
@@ -15,5 +16,21 @@ namespace courseProject.Models
     {
         [Required][UIHint("email")] public string Email { get; set; }
         [Required][UIHint("password")] public string Password { get; set; }
+    }
+
+    public class RoleEditModel
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<User> Members { get; set; }
+        public IEnumerable<User> NonMembers { get; set; }
+    }
+
+    public class RoleModeficationModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public string RoleId { get; set; }
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
     }
 }
