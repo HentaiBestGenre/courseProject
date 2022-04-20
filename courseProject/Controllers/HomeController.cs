e0578ea7fbc6c633edd7ccd94ce229ae91eb2fdf
Hentai <97235307+HentaiBestGenre@users.noreply.github.com>
2022-04-20 19:59:46 +0300

    Initial commit

--- courseProject/Controllers/HomeController.cs
+++ courseProject/Controllers/HomeController.cs
@@ -15,11 +15,23 @@ namespace courseProject.Controllers
         [Route("~/Home/")]
         [Route("~/Home/Index")]
         [Authorize]
-        public IActionResult Index() => View();
+        public IActionResult Index() => View(GetData(nameof(Index)));
 
         public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+        [Authorize(Roles = "Users")]
+        public IActionResult OthresAction() => View("Index", GetData(nameof(OthresAction)));
+        
+        private Dictionary<string, object> GetData(string actionName) => new Dictionary<string, object>
+        {
+            ["Action"] = actionName,
+            ["User"] = HttpContext.User.Identity.Name,
+            ["Authenticated"] = HttpContext.User.Identity.IsAuthenticated,
+            ["Auth Type"] = HttpContext.User.Identity.AuthenticationType,
+            ["In Users Role"] = HttpContext.User.IsInRole("Users")
+        };
     }
 }
---
+++ courseProject/Controllers/RoleAdminController.cs
@@ -0,0 +1,103 @@
+﻿using courseProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace courseProject.Controllers
+{
+    public class RoleAdminController : Controller
+    {
+        private RoleManager<IdentityRole> roleManager;
+        private UserManager<User> userManager;
+
+        public RoleAdminController(RoleManager<IdentityRole> roleMng, UserManager<User> userMng) 
+        {
+            roleManager = roleMng;
+            userManager = userMng;
+        }
+
+        [Route("/RoleAdmin/")]
+        public ViewResult Index() => View(roleManager.Roles);
+        public IActionResult Create() => View();
+        
+        [HttpPost]
+        public async Task<IActionResult> Create([Required]string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded) { return RedirectToAction("Index"); }
+                else { AddErrorsFromResult(result); }
+            }
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await roleManager.DeleteAsync(role);
+                if (result.Succeeded) { return RedirectToAction("Index"); }
+                else { AddErrorsFromResult(result); }
+            }
+            else { ModelState.AddModelError("", "No role found"); }
+            return View("Index", roleManager.Roles);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            List<User> members = new List<User>();
+            List<User> nonMembers = new List<User>();
+            foreach(User user in userManager.Users)
+            {
+                var list = await userManager.IsInRoleAsync(user, role.Name) ? members : nonMembers; /////// Error
+                list.Add(user);
+            }
+            return View(new RoleEditModel {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RoleModeficationModel model)
+        {
+            IdentityResult result;
+            if (ModelState.IsValid)
+            {
+                foreach(string userId in model.IdsToAdd ?? new string[] { })
+                {
+                    User user = await userManager.FindByIdAsync(userId);
+                    if(user != null)
+                    {
+                        result = await userManager.AddToRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded) { AddErrorsFromResult(result); }
+                    }
+                }
+                foreach(string userId in model.IdsToDelete ?? new string[] { })
+                {
+                    User user = await userManager.FindByIdAsync(userId);
+                    if( user != null)
+                    {
+                        result = await userManager.RemoveFromRoleAsync(user, model.RoleName);
+                        if(!result.Succeeded) { AddErrorsFromResult(result); }
+                    }
+                }
+            }
+            if (ModelState.IsValid) { return RedirectToAction(nameof(Index)); }
+            else { return await Edit(model.RoleId); }
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+    }
+}
